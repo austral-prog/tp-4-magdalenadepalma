f5b6379852ea2b3900eb82abc83c9a937d7d0c2c
magdalenadepalma <mdepalma@mail.austral.edu.ar>
2025-04-14 15:11:39 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+    if año/4 and (año%4 == 0):
+	    print(f"El año {año} es bisiesto")
+    elif año/400 and (año%400 == 0):
+	    print(f"El año {año} es bisiesto")
+    else:
+	    print(f"El año {año} no es bisiesto")
